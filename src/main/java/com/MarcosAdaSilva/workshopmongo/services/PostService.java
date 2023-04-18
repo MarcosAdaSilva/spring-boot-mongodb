36094160667b786d59042adc33e3ae3756dca86c
MarcosAdaSilva <marcaofly@gmail.com>
2023-04-18 07:39:30 -0300

    Consulta simples com query methods

--- src/main/java/com/MarcosAdaSilva/workshopmongo/services/PostService.java
+++ src/main/java/com/MarcosAdaSilva/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.MarcosAdaSilva.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +21,9 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 
      }
+	
+	public List<Post> finfByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 
 }
